e659972bd8fc0e1bc50b0ce412c4a899273ce858
Femi <femzy14@gmail.com>
2025-10-27 08:08:12 +0100

    first commit

--- src/shared/components/gallery/GallerySkleton.tsx
+++
@@ -1,9 +0,0 @@
-export default function GallerySkeleton() {
-  return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 animate-pulse p-6">
-      {Array.from({ length: 8 }).map((_, index) => (
-        <div key={index} className="aspect-[3/4] rounded-lg bg-gray-400"></div>
-      ))}
-    </div>
-  );
-}
